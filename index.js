4723339a06134eb0a8c481dc906defd1ebb50b9f
avs <98419107+dev-avs@users.noreply.github.com>
2026-02-10 22:29:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Client, GatewayIntentBits } = require("discord.js");
-
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -8,13 +7,19 @@ const client = new Client({
   ]
 });
 
-client.once("clientReady", () => {
+client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
-  await message.channel.send("My name is mangoes**bot** 67 is so funny!!");
+
+  const reply = await message.reply("My name is mangoes**bot** 67 is so funny!!");
+
+  await reply.react("6️⃣");
+  await reply.react("7️⃣");
+  await reply.react("🥭");
+  await reply.react("🤖");
 });
 
 client.login(process.env.TOKEN);
